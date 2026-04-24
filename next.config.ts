caf3777c55d6ace7eefd1121c607a07ca3d1a448
Narni Venkata Viswesh <viswasumo@gmail.com>
2026-04-24 10:33:34 +0530

    eszlinyissue

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   allowedDevOrigins: ["192.168.29.128"],
-  
 };
 
 export default nextConfig;
